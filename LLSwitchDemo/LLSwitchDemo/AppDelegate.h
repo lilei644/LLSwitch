82c3803ebf7599f6a6d450dffbcd64fbfa89f182
lilei644 <1793161563@qq.com>
2016-05-17 15:08:53 +0800

    upload
    upload

---
+++ LLSwitchDemo/LLSwitchDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LLSwitchDemo
+//
+//  Created by admin on 16/5/17.
+//  Copyright © 2016年 LiLei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
